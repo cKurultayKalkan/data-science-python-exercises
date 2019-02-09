994a7fe9a1c1c551a49141b654d8619b61f9407b
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-02-09 13:42:36 +0300

    List Comprehension

--- panda-exercise.py
+++ panda-exercise.py
@@ -3,7 +3,7 @@ import numpy as np
 
 dictionary = {"NAME": ["ali", "veli", "kenan", "hilal", "ayse", "evren"],
               "AGE": [15, 16, 17, 18, 19, 20],
-              "MAAS": [100, 200, 300, 400, 500, 600]}
+              "SALARY": [100, 200, 300, 400, 500, 600]}
 
 dataFrame1 = pd.DataFrame(dictionary)
 
@@ -19,21 +19,29 @@ print(dataFrame1.describe())
 print(dataFrame1["AGE"])
 print(dataFrame1.AGE)
 
-dataFrame1["yeni_feature"] = [-1, -2, -3, -4, -5, -6]
+dataFrame1["new_feature"] = [-1, -2, -3, -4, -5, -6]
 print(dataFrame1.loc[:, "AGE"])
 print(dataFrame1.loc[:3, "AGE"])
 
-filter1 = dataFrame1.MAAS > 200
+filter1 = dataFrame1.SALARY > 200
 
 filtered = dataFrame1[filter1]
-
 print(filtered)
 
 filter2 = dataFrame1.AGE < 18
-
 filtered = dataFrame1[filter1 & filter2]
-
 print(filtered)
 
+average = dataFrame1.SALARY.mean()
+print(average)
 
+average_np = np.mean(dataFrame1.SALARY)
+print(average_np)
 
+dataFrame1["salary_level"] = ["high" if salary > average else "low" for salary in dataFrame1.SALARY]
+
+print(dataFrame1)
+
+dataFrame1.columns = [each.lower() for each in dataFrame1.columns]
+
+print(dataFrame1)

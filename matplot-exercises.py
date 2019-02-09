19068a64968654b9c86997fd633ecca40b67f9be
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-02-09 16:24:36 +0300

    line plot

--- matplot-exercises.py
+++ matplot-exercises.py
@@ -20,5 +20,28 @@ print(df.describe())
 
 setosa = df[df.Species == "Iris-setosa"]
 versicolor = df[df.Species == "Iris-versicolor"]
+
 print(setosa.describe())
 print(versicolor.describe())
+
+# plot
+import matplotlib.pyplot as plt
+
+df1 = df.drop(["Id"], axis=1)
+print(df1)
+
+df1.plot()
+plt.show()
+
+setosa = df[df.Species == "Iris-setosa"]
+versicolor = df[df.Species == "Iris-versicolor"]
+virginica = df[df.Species == "Iris-virginica"]
+
+plt.plot(setosa.Id, setosa.PetalLengthCm, color="red", label="setosa")
+plt.plot(versicolor.Id, versicolor.PetalLengthCm, color="green", label="versicolor")
+plt.plot(virginica.Id, virginica.PetalLengthCm, color="blue", label="virginica")
+plt.legend()
+plt.xlabel('Id')
+plt.ylabel('Petal Length Cm')
+df1.plot(grid=True, linestyle=":", alpha=0.5)
+plt.show()

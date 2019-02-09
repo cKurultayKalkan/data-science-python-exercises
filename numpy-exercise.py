8161f7b122246f122c28d1a16e5452682cf3d60b
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-02-09 05:57:46 +0300

    reshape, resize, transpoze, ravel

--- numpy-exercise.py
+++ numpy-exercise.py
@@ -3,3 +3,21 @@ import numpy as np
 array = np.array([[1, 2, 3, 4, 5, 6, ], [7, 8, 9, 10, 11, 12]])
 
 print(array[:, 1:4])
+
+array = np.array([[1, 2, 3, 4, 5, 6, ], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18]])
+
+print(array.ravel())
+
+array2 = array.reshape(6, 3)
+
+print(array2)
+
+array3 = array.T
+
+print(array3)
+
+array5 = np.array([[1, 2], [3, 4], [5, 6]])
+
+array5.resize((2, 3))
+
+print(array5)

da31cb20e1fb397f91e48f55d50ef834990e448d
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-02-09 13:31:43 +0300

    dataframe filtering

--- panda-exercise.py
+++ panda-exercise.py
@@ -15,3 +15,25 @@ print(dataFrame1.columns)
 print(dataFrame1.info())
 print(dataFrame1.dtypes)
 print(dataFrame1.describe())
+
+print(dataFrame1["AGE"])
+print(dataFrame1.AGE)
+
+dataFrame1["yeni_feature"] = [-1, -2, -3, -4, -5, -6]
+print(dataFrame1.loc[:, "AGE"])
+print(dataFrame1.loc[:3, "AGE"])
+
+filter1 = dataFrame1.MAAS > 200
+
+filtered = dataFrame1[filter1]
+
+print(filtered)
+
+filter2 = dataFrame1.AGE < 18
+
+filtered = dataFrame1[filter1 & filter2]
+
+print(filtered)
+
+
+

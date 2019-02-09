7c2af536b1177e2fb26374c9e464511a8892da2c
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-02-09 12:36:54 +0300

    first panda, dataframe

---
+++ panda-exercise.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+dictionary = {"NAME": ["ali", "veli", "kenan", "hilal", "ayse", "evren"],
+              "AGE": [15, 16, 17, 18, 19, 20],
+              "MAAS": [100, 200, 300, 400, 500, 600]}
+
+dataFrame1 = pd.DataFrame(dictionary)
+
+head = dataFrame1.head()
+
+tail = dataFrame1.tail()
